1768a02ed3a1e77bef9b99ef90f70667b94715a3
svankappel <sylvestrevk@gmail.com>
2024-10-11 14:05:03 +0200

    dtls for later

--- src/main.c
+++ src/main.c
@@ -25,8 +25,13 @@ int main(void)
 	profiler_init();
 
 	wifi_init();
+
+	wifi_ps_mode_legacy();
+	wifi_ps_set_listen_interval(10);
+	wifi_ps_wakeup_listen_interval();
+	wifi_ps_disable();
+
 	wifi_connect();
-	
 
 	coap_init();
 
@@ -34,7 +39,29 @@ int main(void)
 
 	while(true)
 	{
-		k_sleep(K_SECONDS(1));
+		//power save
+		profiler_ch0_set();
+		wifi_ps_enable();
+
+		for(int i = 0; i < 5; i++)
+		{
+			uint8_t message[25];
+			sprintf(message,"{\"sensor-value\":%d}",sys_rand8_get());	
+			coap_put("validate", "message", strlen(message));
+			k_sleep(K_SECONDS(4));
+		}
+
+		//no power save
+		profiler_ch0_clear();
+		wifi_ps_disable();
+
+		for(int i = 0; i < 5; i++)
+		{
+			uint8_t message[25];
+			sprintf(message,"{\"sensor-value\":%d}",sys_rand8_get());	
+			coap_put("validate", "message", strlen(message));
+			k_sleep(K_SECONDS(4));
+		}
 	}
 
 
