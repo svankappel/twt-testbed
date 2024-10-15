a200b72cda41b3dbea43b4e448ba82a7300ee1d5
svankappel <sylvestrevk@gmail.com>
2024-10-15 09:53:40 +0200

    removed coap client (comment in cmakelists, prjconf)

--- src/main.c
+++ src/main.c
@@ -13,42 +13,22 @@
 #include <zephyr/random/random.h>
 
 #include "wifi/wifi_sta.h"
-#include "coap.h"
+//#include "coap.h"
 #include "profiler.h"
 
-
 LOG_MODULE_REGISTER(main, CONFIG_MY_MAIN_LOG_LEVEL);
-
-static void handle_twt_event(const int awake)
-{
-	if (awake) {
-		LOG_INF("TWT event: Awake");
-	} else {
-		LOG_INF("TWT event: Asleep");
-	}
-}
-
 
 int main(void)
 {
 	profiler_init();
-
 	wifi_init();
-
-	wifi_twt_register_event_callback(handle_twt_event);
-
-	wifi_ps_disable();
-
 	wifi_connect();
 
-	while(true)
-	{
-		wifi_twt_setup(50, 1000);
-		k_sleep(K_SECONDS(20));
-		wifi_twt_teardown();
-		k_sleep(K_SECONDS(20));
-	}
+	//static struct lwm2m_ctx client;
 
+	while (true)
+	{
+	}
 
 	return 0;
 }
