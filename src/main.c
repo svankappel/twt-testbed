ee4a65c0163634d64a83066c936da893f85dfb5b
svankappel <sylvestrevk@gmail.com>
2024-10-21 11:06:06 +0200

    .

--- src/main.c
+++ src/main.c
@@ -25,74 +25,86 @@ K_SEM_DEFINE(test_sem, 0, 1);
 
 int main(void)
 {
-        int ret;
+    int ret;
 
-        LOG_INF("Starting TWT testbench ...");
+    LOG_INF("Starting TWT testbench ...");
 
-        // initialize setup
-	profiler_init();
-        if(ret != 0)
-        {
-            LOG_ERR("Failed to initialize profiler");
-            k_sleep(K_FOREVER);
-        }
+    // initialize setup
+    ret = profiler_init();
+    if(ret != 0)
+    {
+        LOG_ERR("Failed to initialize profiler");
+        k_sleep(K_FOREVER);
+    }
 
-	wifi_init();
+    wifi_init();
 
-	ret = wifi_ps_disable();
-        if(ret != 0)
-        {
-            LOG_ERR("Failed to disable power save mode");
-            k_sleep(K_FOREVER);
-        }
+    ret = wifi_ps_disable();
+    if(ret != 0)
+    {
+        LOG_ERR("Failed to disable power save mode");
+        k_sleep(K_FOREVER);
+    }
 
-        ret = wifi_connect();
-        if(ret != 0)
-        {
-            LOG_ERR("Failed to connect to wifi");
-            k_sleep(K_FOREVER);
-        }
+    ret = wifi_connect();
+    if(ret != 0)
+    {
+        LOG_ERR("Failed to connect to wifi");
+        k_sleep(K_FOREVER);
+    }
 
-        coap_init();
-        if(ret != 0)
-        {
-            LOG_ERR("Failed to initialize CoAP client");
-            k_sleep(K_FOREVER);
-        }
+    coap_init();
+    if(ret != 0)
+    {
+        LOG_ERR("Failed to initialize CoAP client");
+        k_sleep(K_FOREVER);
+    }
 
-        wifi_disconnect();
-        if(ret != 0)
-        {
-            LOG_ERR("Failed to disconnect from wifi");
-            k_sleep(K_FOREVER);
-        }
+    k_sleep(K_SECONDS(1));
 
-        k_sleep(K_SECONDS(1));
 
-        LOG_INF("TWT testbench initialized. Initializing tests ...");
+    coap_put("test/test1","{\"sensor-value\":0}",4000);
+    k_sleep(K_SECONDS(3));
+    coap_put("test/test1","{\"sensor-value\":0}",4000);
+    k_sleep(K_SECONDS(3));
+    coap_put("test/test1","{\"sensor-value\":0}",4000);
+
+    k_sleep(K_FOREVER);
         
 
-        // initialize the tests
-
-        struct test_sensor_twt_settings test_settings_1 = {
-                .twt_interval = 100,
-                .twt_wake_interval = 1000,
-                .test_number = 1
-        };
-        init_test_sensor_twt(&test_sem, &test_settings_1);
-
-        struct test_sensor_twt_settings test_settings_2 = {
-                .twt_interval = 100,
-                .twt_wake_interval = 2000,
-                .test_number = 2
-        };
-        init_test_sensor_twt(&test_sem, &test_settings_2);
-
-        LOG_INF("Tests initialized. Starting tests ...");
-
-
-        // give semaphore to start the tests
-        k_sem_give(&test_sem);
+    wifi_disconnect();
+    if(ret != 0)
+    {
+        LOG_ERR("Failed to disconnect from wifi");
         k_sleep(K_FOREVER);
-	return 0;
+    }
+
+    k_sleep(K_SECONDS(1));
+
+    LOG_INF("TWT testbench initialized. Initializing tests ...");
+
+
+    // initialize the tests
+
+    struct test_sensor_twt_settings test_settings_1 = {
+            .twt_interval = 100,
+            .twt_wake_interval = 1000,
+            .test_number = 1
+    };
+    init_test_sensor_twt(&test_sem, &test_settings_1);
+
+    struct test_sensor_twt_settings test_settings_2 = {
+            .twt_interval = 100,
+            .twt_wake_interval = 2000,
+            .test_number = 2
+    };
+    init_test_sensor_twt(&test_sem, &test_settings_2);
+
+    LOG_INF("Tests initialized. Starting tests ...");
+
+
+    // give semaphore to start the tests
+    k_sem_give(&test_sem);
+    k_sleep(K_FOREVER);
+    return 0;
 }
