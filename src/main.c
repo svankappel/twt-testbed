e1fb19b5ca9a2bd9f9d3f48efeb7a9aff3e65c44
svankappel <sylvestrevk@gmail.com>
2024-10-23 17:34:44 +0200

    wifi disco co

--- src/main.c
+++ src/main.c
@@ -19,11 +19,34 @@
 #include "profiler.h"
 #include "tests/test_sensor_twt.h"
 
-
 LOG_MODULE_REGISTER(main, CONFIG_MY_MAIN_LOG_LEVEL);
+K_THREAD_STACK_DEFINE(test_thread_stack, 4096);
 
 K_SEM_DEFINE(test_sem, 0, 1);
 
+int d;
+struct k_thread debug_thread_data;
+
+void coap_debug_thread(void)
+{
+    k_sem_take(&test_sem, K_FOREVER);
+
+    wifi_connect();
+
+    d = 0;
+    char buf[32];
+    snprintf(buf, sizeof(buf), "payload");
+
+    while (true)
+    {
+        snprintf(buf, sizeof(buf), "test-payload-%d", d++);
+        coap_put("test/test1", buf, 1000);
+        snprintf(buf, sizeof(buf), "test-payload-%d", d++);
+        coap_put("test/test1", buf, 1000);
+
+        k_sleep(K_SECONDS(5));
+    }
+}
 
 int main(void)
 {
@@ -39,30 +62,29 @@ int main(void)
     wifi_ps_disable();
     
     wifi_connect();
- 
 
     coap_init();
 
+    k_sleep(K_SECONDS(1));
+
+    
     ret = coap_validate();
-    if(ret != 0)
+    if (ret != 0)
     {
         LOG_ERR("Failed to validate coap");
         k_sleep(K_FOREVER);
     }
 
-    k_sleep(K_SECONDS(1));
+    wifi_disconnect();
 
-    int i = 0;
-    char buf[32];
+    k_sleep(K_SECONDS(5));
 
-    while(true)
-    {
-        snprintf(buf, sizeof(buf), "test-payload-%d", i++);
-        coap_put("test/test1", buf, 1000);
-        snprintf(buf, sizeof(buf), "test-payload-%d", i++);
-        coap_put("test/test1", buf, 1000);
 
-        k_sleep(K_SECONDS(5));
-        
-    }
+    k_tid_t test_thread_id = k_thread_create(&debug_thread_data, test_thread_stack ,4096, coap_debug_thread, NULL, NULL, NULL, 7, 0, K_NO_WAIT);
+    k_thread_name_set(test_thread_id, "test_thread");
+    k_thread_start(test_thread_id);
+
+    k_sem_give(&test_sem);
+
+    k_sleep(K_FOREVER);
 }
