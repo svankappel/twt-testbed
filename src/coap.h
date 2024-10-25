55d22953f067534a58efe0f4653a42b5a3662d22
svankappel <sylvestrevk@gmail.com>
2024-10-25 09:39:59 +0200

    get stat on server

--- src/coap.h
+++ src/coap.h
@@ -8,6 +8,8 @@ int coap_init();
 
 int coap_validate();
 
+int coap_get_stat();
+
 void coap_register_response_callback(void (*callback)(int16_t code, void * user_data),void * callback_user_data);
 
 int coap_put(const char *resource,uint8_t *payload, uint32_t timeout);
